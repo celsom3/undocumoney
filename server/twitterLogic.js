9e579d7a8bbba5f38b1a0df4fad1168659bab235
Celso Mireles Martinez <celso.mireles@gmail.com>
2015-08-30 16:27:54 -0700

    Trying something on twitterLogic

--- server/twitterLogic.js
+++ server/twitterLogic.js
@@ -1,51 +1,64 @@
 Meteor.startup(function(){
   Meteor.methods({
     'getLatestTweets': function(){
-
-
-      var cleanTweets = T.get('search/tweets', { q: '#undocumoney since:2015-08-27', count: 40, f: 'images' }, Meteor.bindEnvironment(function(err, data, response) {
-        if(!err){
+      var cleanTweets = T.get('search/tweets', { q: '#undocumoney since:2015-08-28', count: 1, f: 'images' }, Meteor.bindEnvironment(function(err, data, response) {
+        var statuses = data.statuses ? data.statuses : [];
+        //console.log(data);
+        if(!err && statuses.length !== 0){
           console.log("Getting tweets...");
-          //console.log(response);
-          // var statusText = data.statuses[1].text;
-          // console.log(statusText);
-          //
-          // var imageUrl = data.statuses[1].entities.media[0].media_url;
-          // console.log(imageUrl);
-          //
-          // var screenName = data.statuses[1].user.screen_name;
-          // console.log(data.statuses[1].user.screen_name);
+          console.log(" ");
+          console.log(" ");
+          console.log(" ");
+          console.log(" ");
+
           var formattedTweets = [];
 
           // Initialize counter for tweets with images
           var foundImages = 0;
 
           // for (each tweet && counter<=4)
-          for ( var i = 0; foundImages < 10; i++){
+          for ( var i = 0; foundImages < 20; i++){
             // Check to see if status already exists in collection
             var already_have = false;
 
+            // var thisTweetId = statuses[i].id ? statuses[i].id : 0;
+            //
+            // for (b = 0; b < tweets.find().fetch().length; b++){
+            //   if(tweets.find().fetch()[b].id === thisTweetId){
+            //     already_have = true;
+            //     break;
+            //   }
+            // }
+            // console.log(statuses[i]);
+            // var thisTweetText = statuses[i].text;
+            //
+            // for (c = 0; c < tweets.find().fetch().length; c++){
+            //   if(tweets.find().fetch()[c].text === thisTweetText){
+            //     already_have = true;
+            //     break;
+            //   }
+            // }
 
-            for (b = 0; b < tweets.find().fetch().length; b++){
-              if(tweets.find().fetch()[b].statusText === statusText){
-                already_have = true;
-              }
-            }
+            console.log(statuses[i]);
 
             // If tweet has image
-            if(data.statuses[i].entities.media){
-              // Get all needed tweet info and add to tweet array
-              var statusText = data.statuses[i].text;
+            if((statuses[i].entities !== undefined) && (statuses[i].entities.media !== undefined) && !already_have){
+              // Get all needed tweet info and add to tweet db
+              // Get the tweet ID
+              var tweetId = statuses[i].id;
+
+              var statusText = statuses[i].text;
               //console.log(statusText);
 
-              var imageUrl = data.statuses[i].entities.media[0].media_url;
+              var imageUrl = statuses[i].entities.media[0].media_url;
               //console.log(imageUrl);
 
-              var screenName = data.statuses[i].user.screen_name;
-              //console.log(data.statuses[i].user.screen_name);
+              var screenName = statuses[i].user.screen_name;
+              //console.log(statuses[i].user.screen_name);
+
 
               formattedTweets.push({
-
+                tweetId: tweetId,
                 statusText: statusText,
                 imageUrl: imageUrl,
                 screenName: screenName
@@ -53,28 +66,28 @@ Meteor.startup(function(){
 
               tweets.insert({
                 timestamp: new Date(),
+                tweetId: tweetId,
                 statusText: statusText,
                 imageUrl: imageUrl,
                 screenName: screenName
               });
 
-
-
-              foundImages++;
+              foundImages = foundImages + 1;
             }
 
           }// END FOR loop
 
 
-          console.log(formattedTweets);
-          return formattedTweets;
+          //console.log(formattedTweets);
+          // return formattedTweets;
         }else{
           console.log(err);
           //return err;
         }
+
       }));
 
-      return cleanTweets;
+      // return cleanTweets;
     },
     'insert_tweet': function(tweet){
       this.unblock();
