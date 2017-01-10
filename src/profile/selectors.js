21994afb9bac1eaa373bab385b8c09f24551c6dc
Celso Mireles <celso.mireles@gmail.com>
2017-01-09 18:40:18 -0700

    remove unused components

--- src/profile/selectors.js
+++
@@ -1,9 +0,0 @@
-export const getProfile = (state) => state.auth.profile;
-export const getInitialCheck = (state) => {
-    if (state.auth.profile.user_metadata && state.auth.profile.user_metadata.initialCheck){
-        return state.auth.profile.user_metadata.initialCheck;
-    }
-    else {
-        return false;
-    }
-};
